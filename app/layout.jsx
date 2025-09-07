01b7cd75ee931f3414fe4fbc9fda37ffb3f4a0b4
Mehrshad <shadmehrbiriya@gmail.com>
2025-09-08 01:19:08 +0330

    neww

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,14 +1,14 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { JetBrains_Mono, JetBrains_Mono } from "next/font/google";
 import "./globals.css";
+import { Weight } from "lucide-react";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// Components
+import Header from "../components/Header";
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const JetBrainsMono = JetBrains_Mono({
   subsets: ["latin"],
+  Weight: ["100, 200, 300, 400, 500, 600, 700, 800"],
+  variable: "--font-jetbrainsMono",
 });
 
 export const metadata = {
@@ -19,9 +19,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={JetBrainsMono.variable}>
+        <Header />
         {children}
       </body>
     </html>
---
+++ components/Header.jsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+import { Button } from "./ui/button";
+
+
+
+const Header = () => {
+  return <Header>header</Header>;
+};
+export default Header;
