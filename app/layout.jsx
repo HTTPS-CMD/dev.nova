27fa1bcf0e28594ff2b5db7e51e91081a5d0733b
Mehrshad <shadmehrbiriya@gmail.com>
2025-09-08 14:31:30 +0330

    n
    newwww

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,6 +4,8 @@ import { Weight } from "lucide-react";
 
 // Components
 import Header from "../components/Header";
+import PageTransition from "@/components/PageTransition";
+import StairTransition from "@/components/StairTransition";
 
 const JetBrainsMono = JetBrains_Mono({
   subsets: ["latin"],
@@ -21,7 +23,8 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={JetBrainsMono.variable}>
         <Header />
-        {children}
+        <StairTransition />
+        <PageTransition>{children}</PageTransition>
       </body>
     </html>
   );
